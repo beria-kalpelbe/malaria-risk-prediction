f00239211af4bcb6a4859f2e196589021215344e
beria-kalpelbe <beriakalpelbe@gmail.com>
2024-08-11 01:02:28 +0200

    extracting climate data from ERA5

---
+++ src/climate-data/main.py
@@ -0,0 +1,12 @@
+import json
+
+with open("../utils/cdsapi-client.json", "r") as f:
+    cdsapi_data = json.load(f)
+
+url = cdsapi_data["url"]
+key = cdsapi_data["key"]
+
+from climate_data import ClimateData
+
+dataset = ClimateData(url=url, key=key)
+dataset.run()
