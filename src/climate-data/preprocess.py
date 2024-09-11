59378383d912835bfefce88e0371e23b298e2a87
beria-kalpelbe <beriakalpelbe@gmail.com>
2024-09-11 17:02:05 +0100

    Data preprocessing and data description

--- src/climate-data/preprocess.py
+++ src/climate-data/preprocess.py
@@ -3,6 +3,7 @@ import numpy as  np
 import geopandas as gpd
 import pandas as pd
 from shapely.geometry import Point
+from tqdm import tqdm
 
 original_climate_data = xr.open_dataset('../../data/chad-data-climate.nc')
 malaria_data = pd.read_excel('../../data/malaria-data.xlsx')
@@ -25,48 +26,55 @@ def aggregate_data(data_raster, var_name, time, data, map_data):
     
 map_data = gpd.read_file('../../data/chadgeodata/geoBoundaries-TCD-ADM1-all/geoBoundaries-TCD-ADM1.geojson')
 
-# for time in tqdm(data.valid_time, desc='Aggregating climate data'):
-time = original_climate_data.valid_time[0]
-climate_data = pd.DataFrame(map_data['shapeName'], columns=['time','shapeName','t2m', 'tp', 'lai_hv', 'lai_lv'])
-climate_data = aggregate_data(original_climate_data, 't2m', str(time.values),climate_data, map_data)
-climate_data = aggregate_data(original_climate_data, 'tp', str(time.values), climate_data, map_data)
-climate_data = aggregate_data(original_climate_data, 'lai_hv', str(time.values), climate_data, map_data)
-climate_data = aggregate_data(original_climate_data, 'lai_lv', str(time.values), climate_data, map_data)
-climate_data['time'] = [str(time.values).split('T')[0]]*23
-
 malaria_data['state'] = malaria_data['state'].replace('BEG', 'BAHR EL GAZEL')
-climate_data = climate_data.rename(columns={'shapeName':'state'})
-climate_data['state'] = climate_data['state'].str.upper()
-climate_data['state'] = climate_data['state'].replace('ENNEDI-OUEST', 'ENNEDI OUEST')
-climate_data['state'] = climate_data['state'].replace('OUADDAÏ', 'OUADDAI')
-climate_data['state'] = climate_data['state'].replace('GUÉRA', 'GUERA')
-climate_data['state'] = climate_data['state'].replace('HADJER-LAMIS', 'HADJER LAMIS')
-climate_data['state'] = climate_data['state'].replace('CHARI-BAGUIRMI', 'CHARI BAGUIRMI')
-climate_data['state'] = climate_data['state'].replace('MAYO KEBBI EST', 'MAYO KEBBI EST')
-climate_data['state'] = climate_data['state'].replace('MAYO-KEBBI OUEST', 'MAYO KEBBI OUEST')
-climate_data['state'] = climate_data['state'].replace('MOYEN-CHARI', 'MOYEN CHARI')
-climate_data['state'] = climate_data['state'].replace('ENNEDI-EST', 'ENNEDI EST')
-climate_data['state'] = climate_data['state'].replace('TANDJILÉ', 'TANDJILE')
-climate_data['state'] = climate_data['state'].replace('MAYO-KEBBI EST', 'MAYO KEBBI EST')
-climate_data['state'] = climate_data['state'].replace("N'DJAMENA REGION", 'NDJAMENA')
-
 malaria_data['time'] = [f'{malaria_data['year'][i]}-{malaria_data['month'][i]:02d}-01' for i in range(len(malaria_data['year']))]
 malaria_data = malaria_data.drop(columns=['year', 'month'])
 
-processed_data = pd.merge(climate_data, malaria_data)
-processed_data = processed_data[['state', 'time', 't2m', 'tp', 'lai_hv', 'lai_lv', 'cases','deaths']]
+data_to_csv = pd.DataFrame()
+for time in tqdm(original_climate_data.valid_time, desc="Aggregating climate data"):
+    map_data = gpd.read_file('../../data/chadgeodata/geoBoundaries-TCD-ADM1-all/geoBoundaries-TCD-ADM1.geojson')
+    climate_data = pd.DataFrame(map_data['shapeName'], columns=['time','shapeName','t2m', 'tp', 'lai_hv', 'lai_lv'])
+    climate_data = aggregate_data(original_climate_data, 't2m', str(time.values),climate_data, map_data)
+    climate_data = aggregate_data(original_climate_data, 'tp', str(time.values), climate_data, map_data)
+    climate_data = aggregate_data(original_climate_data, 'lai_hv', str(time.values), climate_data, map_data)
+    climate_data = aggregate_data(original_climate_data, 'lai_lv', str(time.values), climate_data, map_data)
+    climate_data['time'] = [str(time.values).split('T')[0]]*23
+    # climate_data = pd.concat([climate_data, d], axis=0)
+    # climate_data.reset_index(drop=True, inplace=True)
 
-map_data = map_data.rename(columns={'shapeName':'state'})
+    climate_data = climate_data.rename(columns={'shapeName':'state'})
+    climate_data['state'] = climate_data['state'].str.upper()
+    climate_data['state'] = climate_data['state'].replace('ENNEDI-OUEST', 'ENNEDI OUEST')
+    climate_data['state'] = climate_data['state'].replace('OUADDAÏ', 'OUADDAI')
+    climate_data['state'] = climate_data['state'].replace('GUÉRA', 'GUERA')
+    climate_data['state'] = climate_data['state'].replace('HADJER-LAMIS', 'HADJER LAMIS')
+    climate_data['state'] = climate_data['state'].replace('CHARI-BAGUIRMI', 'CHARI BAGUIRMI')
+    climate_data['state'] = climate_data['state'].replace('MAYO KEBBI EST', 'MAYO KEBBI EST')
+    climate_data['state'] = climate_data['state'].replace('MAYO-KEBBI OUEST', 'MAYO KEBBI OUEST')
+    climate_data['state'] = climate_data['state'].replace('MOYEN-CHARI', 'MOYEN CHARI')
+    climate_data['state'] = climate_data['state'].replace('ENNEDI-EST', 'ENNEDI EST')
+    climate_data['state'] = climate_data['state'].replace('TANDJILÉ', 'TANDJILE')
+    climate_data['state'] = climate_data['state'].replace('MAYO-KEBBI EST', 'MAYO KEBBI EST')
+    climate_data['state'] = climate_data['state'].replace("N'DJAMENA REGION", 'NDJAMENA')
 
-map_data['t2m'] = processed_data['t2m']
-map_data['tp'] = processed_data['tp']
-map_data['lai_hv'] = processed_data['lai_hv']
-map_data['lai_lv'] = processed_data['lai_lv']
-map_data['cases'] = processed_data['cases']
-map_data['deaths'] = processed_data['deaths']
+    processed_data = pd.merge(climate_data, malaria_data)
+    data_to_csv = pd.concat([data_to_csv, processed_data], axis=0)
+    processed_data = processed_data[['state', 'time', 't2m', 'tp', 'lai_hv', 'lai_lv', 'cases','deaths']]
 
-map_data['t2m'] = map_data['t2m'].astype(np.float64)
-map_data['tp'] = map_data['tp'].astype(np.float64)
-map_data['lai_hv'] = map_data['lai_hv'].astype(np.float64)
-map_data['lai_lv'] = map_data['lai_lv'].astype(np.float64)
+    map_data = map_data.rename(columns={'shapeName':'state'})
 
+    map_data['t2m'] = processed_data['t2m']
+    map_data['tp'] = processed_data['tp']
+    map_data['lai_hv'] = processed_data['lai_hv']
+    map_data['lai_lv'] = processed_data['lai_lv']
+    map_data['cases'] = processed_data['cases']
+    map_data['deaths'] = processed_data['deaths']
+
+    map_data['t2m'] = map_data['t2m'].astype(np.float64)
+    map_data['tp'] = map_data['tp'].astype(np.float64)
+    map_data['lai_hv'] = map_data['lai_hv'].astype(np.float64)
+    map_data['lai_lv'] = map_data['lai_lv'].astype(np.float64)
+
+    map_data.to_file(f'../../data/preprocessed_data/{str(time.values).split('T')[0]}.geojson', driver='GeoJSON')
+
+data_to_csv.to_csv('../../data/panel_data.csv', index=False)
